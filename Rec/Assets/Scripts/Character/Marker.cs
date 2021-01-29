2a6cf8b73bd87a0767a9ebf1f703793abfbd79ee
MittaniK1 <m.keichi16256@gmail.com>
2021-01-30 04:34:43 +0900

    add HightDifferenceText

--- Rec/Assets/Scripts/Character/Marker.cs
+++ Rec/Assets/Scripts/Character/Marker.cs
@@ -9,18 +9,29 @@ public class Marker : MonoBehaviour
     [SerializeField] private Image markerImage;
     private GameObject compass;
     private GameObject target;
-    private float markerStrength = 10.0f;
+    private float markerStrength;
+    private EnemyMarkerText enemyMarkerText;
 
     private void Start() {
         compass = GameObject.Find("CompassMask");
         target = GameObject.Find("Main Camera");
+        
         marker = Instantiate(markerImage, compass.transform.transform.position, Quaternion.identity) as Image;
         marker.transform.SetParent(compass.transform, false);
+        enemyMarkerText = marker.gameObject.GetComponent<EnemyMarkerText>();
     }
 
     private void Update() {
+        markerStrength = target.GetComponent<CompassRader>().raderStrength;
+
         Vector3 position = transform.position - target.transform.position;
         position *= markerStrength;
         marker.transform.localPosition = new Vector3(position.x, position.z, 0);
+
+        enemyMarkerText.MarkHight(transform.position.y - target.transform.position.y);
+    }
+
+    private void OnDestroy() {
+        Destroy(marker.gameObject);    
     }
 }
