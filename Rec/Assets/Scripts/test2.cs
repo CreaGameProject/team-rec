dbd585e9e022b48a2eab9f4aee9ca1fc08677167
vanilla5 <72849415+vanilla5@users.noreply.github.com>
2021-03-05 23:47:01 +0900

    SE変更

---
+++ Rec/Assets/Scripts/test2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    /// <summary>
+    /// SystemSoundManager.Instance.PlaySE("流したいSE");
+    /// で効果音を流すことが出来ます。
+    /// </summary>
+    
+    void Start()
+    {
+        //MusicManager.Instance.PlayMusic("ashita");
+    }
+
+    
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            SystemSoundManager.Instance.PlaySE("ショット");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SystemSoundManager.Instance.PlaySE("ビーム砲1");
+        }
+    }
+}
