9005ccada0f7d8c0110e7b3a736e2281fd5e0fbf
putinu <putinu@users.noreply.github.com>
2021-06-27 14:27:00 +0900

    ゲームクリア、スコアボードの実装

---
+++ Rec/Assets/Scripts/UI/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スコア用変数保存クラス
+/// </summary>
+public class Score
+{
+    /// <summary>
+    /// 一般的な敵キルの加算。レーザーキルを含む。
+    /// </summary>
+    public static int NormalKills = 0;
+
+    /// <summary>
+    /// レーザーを用いたキルのみを加算。
+    /// </summary>
+    public static int LazerKills = 0;
+
+    /// <summary>
+    /// プレイヤーの体力残量の管理。
+    /// </summary>
+    public static int HPRemains = 100;
+
+    /// <summary>
+    /// ステージを通して発生する敵の個体数
+    /// </summary>
+    public static int EnemyCounts = 0;
+
+    /// <summary>
+    /// ステージ番号
+    /// </summary>
+    public static int Stage = 1;
+}
---
+++ Rec/Assets/Scripts/UI/ScoreBackground.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBackground : MonoBehaviour
+{
+    [Header("動かすUI")]
+    [SerializeField] private Image moveUp1;
+    [SerializeField] private Image moveDown1;
+    [SerializeField] private Image moveUp2;
+    [SerializeField] private Image moveDown2;
+
+    [Space(20)]
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField] private float speed;
+
+    private const int height = 1080;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    /// <summary>
+    /// 背景図形の移動アニメーションを行う
+    /// </summary>
+    private void Move()
+    {
+        float time = Time.deltaTime;
+        moveUp1.transform.Translate(new Vector3(0, speed * time, 0));
+        moveDown1.transform.Translate(new Vector3(0, -speed * time, 0));
+        moveUp2.transform.Translate(new Vector3(0, speed * time, 0));
+        moveDown2.transform.Translate(new Vector3(0, -speed * time, 0));
+
+        if (moveUp1.rectTransform.anchoredPosition.y >= height)
+        {
+            moveUp1.rectTransform.anchoredPosition += new Vector2(0, -2 * height);
+        }
+        else if (moveDown1.rectTransform.anchoredPosition.y <= -height)
+        {
+            moveDown1.rectTransform.anchoredPosition += new Vector2(0, 2 * height);
+        }
+        else if (moveUp2.rectTransform.anchoredPosition.y >= height)
+        {
+            moveUp2.rectTransform.anchoredPosition += new Vector2(0, -2 * height);
+        }
+        else if (moveDown2.rectTransform.anchoredPosition.y <= -height)
+        {
+            moveDown2.rectTransform.anchoredPosition += new Vector2(0, 2 * height);
+        }
+    }
+}
