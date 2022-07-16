441c12fb649333442057e88aa31f47d9283dfb10
putinu <putinu2521@gmail.com>
2022-07-16 14:19:49 +0900

    GoalEvent作成

---
+++ Rec/Assets/Scripts/Stage/GoalEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Core.Stage
+{
+    public class GoalEvent : SingletonMonoBehaviour<GoalEvent>
+    {
+        [SerializeField] private GameObject scoreBackground;
+        [SerializeField] private GameObject stageClearWindow;
+        
+        public void StageClear()
+        {
+            Debug.Log("Stage Clear");
+            Score.HPRemains = Player.Life;
+            
+            scoreBackground.SetActive(true);
+            stageClearWindow.SetActive(true);
+        }
+    }
+}
