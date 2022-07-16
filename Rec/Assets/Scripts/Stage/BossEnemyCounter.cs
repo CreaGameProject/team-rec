d33d7f71851d16a6aaed720a96ca5f883b220d6a
putinu <putinu2521@gmail.com>
2022-07-16 14:20:03 +0900

    BossEnemyCounter追加

---
+++ Rec/Assets/Scripts/Stage/BossEnemyCounter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Core.Stage
+{
+    public static class BossEnemyCounter
+    {
+        private static int _enemyRemains;
+
+        public static void AddEnemyCount(int count)
+        {
+            // 同時に最後の敵は生成されている前提
+            _enemyRemains += count;
+            Debug.Log($"Current Boss Enemy is : {_enemyRemains}");
+        }
+
+        public static int GetEnemyCount() => _enemyRemains;
+
+        public static void DecreaseEnemy()
+        {
+            _enemyRemains--;
+            if (_enemyRemains <= 0)
+            {
+                GoalEvent.Instance.StageClear();
+            }
+        }
+    }
+}
