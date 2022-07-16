fa2b1f16a605ea03c40bd639f2692401aa4d317a
putinu <putinu2521@gmail.com>
2022-07-16 11:30:43 +0900

    SummonTaskBasedBossMarker作成

---
+++ Rec/Assets/Scripts/Stage/SummonTaskBasedBossMarker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Enemy.TaskBased;
+using UnityEngine;
+
+namespace Core.Stage
+{
+    /// <summary>
+    /// SummonTaskBasedEnemyMarkerの最終地点版。この敵を全部倒すとクリアに向かう。
+    /// </summary>
+    public class SummonTaskBasedBossMarker : StageEventMarker
+    {
+        [SerializeField] private float time;
+        [SerializeField] private GameObject original;
+        [Header("if task holder is none, it refer to children.")] [SerializeField] private TaskHolder taskHolder;
+    
+        public override IStageEvent ToStageEvent()
+        {
+            var holder = taskHolder;
+            if (holder == null)
+            {
+                holder = GetComponentInChildren<TaskHolder>();
+            }
+
+            var e = new SummonTaskBasedBossEvent(original, holder.CollectTasks(), transform.position, transform.rotation, time);
+        
+            return e;
+        }
+
+        class SummonTaskBasedBossEvent : IStageEvent
+        {
+            private readonly GameObject _original;
+            private readonly List<IEnemyTask> _tasks;
+            private readonly Vector3 _position;
+            private readonly Quaternion _quaternion;
+
+            public SummonTaskBasedBossEvent(GameObject enemyObject, IEnumerable<IEnemyTask> tasks,
+                Vector3 position, Quaternion quaternion, float time)
+            {
+                _original = enemyObject;
+                _tasks = tasks.ToList();
+                _position = position;
+                _quaternion = quaternion;
+                Time = time;
+            }
+            
+            public void Call()
+            {
+                var clone = Instantiate(_original, _position, _quaternion);
+                var component = clone.GetComponent<TaskBasedEnemy>();
+                component.SetTasks(_tasks);
+            }
+
+            public float Time { get; set; }
+        }
+    }
+}
