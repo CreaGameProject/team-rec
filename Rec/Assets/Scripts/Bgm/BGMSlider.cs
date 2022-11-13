2fb3fa3594644363d8f5d1fa30eb07fcd371e8dc
putinu <putinu2521@gmail.com>
2022-11-13 17:13:25 +0900

    BGMSlider作成

---
+++ Rec/Assets/Scripts/Bgm/BGMSlider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BGMSlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    
+    private void Start()
+    {
+        slider.onValueChanged
+            .AddListener(MusicManager.Instance.ChangeBGMVolume);
+    }
+}
