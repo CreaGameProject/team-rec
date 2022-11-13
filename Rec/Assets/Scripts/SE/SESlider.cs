4aff8d9efc6fc2beb089058ab723c76d410bf441
putinu <putinu2521@gmail.com>
2022-11-13 17:14:20 +0900

    SESlider追加

---
+++ Rec/Assets/Scripts/SE/SESlider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SESlider : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    
+    private void Start()
+    {
+        slider.onValueChanged
+            .AddListener(SystemSoundManager.Instance.ChangeVolume);
+    }
+}
